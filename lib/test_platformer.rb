26c96e1330e75cbc84bca5b08eba416405ebad94
Anthony Cook <antrafcook@gmail.com>
2018-05-03 12:42:14 -0400

    started gravity and jump functions, added jump and run images

--- lib/test_platformer.rb
+++ lib/test_platformer.rb
@@ -4,10 +4,19 @@ require 'gosu'
 module TestPlatformer
   # Your code goes here...
   class Player
+    attr_reader :x, :y, :vy
+
     def initialize
-      @image = Gosu::Image.new('./assets/images/sonic.png')
-      @x = @y = @vel_x = @vel_y = 0.0
+      @x = @y = @vy = 0
       @dir = :right
+      @stand = Gosu::Image.new('./assets/images/sonic_stand.png')
+      @jump = Gosu::Image.new('./assets/images/sonic_jump.gif')
+      @run = Gosu::Image.new('./assets/images/sonic_run.gif')
+      @pose = @stand
+    end
+
+    def floor?
+      self.y === 333
     end
 
     def warp(x,y)
@@ -26,10 +35,15 @@ module TestPlatformer
         offs_x = 25
         factor = -0.3
       end
-      @image.draw(@x + offs_x, @y - 49, 0, factor, 0.3)
+      @pose.draw(@x + offs_x, @y - 49, 0, factor, 0.3)
     end
 
     def update(move_x)
+      if (@vy < 0)
+        @pose = @jump
+      else
+        @pose = @stand
+      end
       # Directional walking, horizontal movement
       if move_x > 0
         @dir = :right
@@ -39,19 +53,32 @@ module TestPlatformer
         @dir = :left
         (-move_x).times { @x -= 1 }
       end
+
+      #gravity
+      @vy += 1
+
+      #Vertical movement
+
+      if @vy > 0
+        @vy.times { @y += 1} until self.floor?
+      end
+      if @vy < 0
+        (-@vy).times {  @y -= 1 }
+      end
+
+    end
+    def jump
+      @vy = -20
     end
   end
 
+
   class Window < Gosu::Window
     def initialize
-      super 640, 480
+      super 700, 480
       self.caption = "Testing..."
       @sonic = Player.new
-      @sonic.warp(70, 115)
-    end
-
-    def draw
-      @sonic.draw
+      @sonic.warp(70, 333)
     end
 
     def update
@@ -60,6 +87,23 @@ module TestPlatformer
       move_x += 5 if Gosu.button_down? Gosu::KB_RIGHT
       @sonic.update(move_x)
     end
+
+    def draw
+      @sonic.draw
+    end
+
+    def button_down(id)
+      case id
+      when Gosu::KB_UP
+        @sonic.jump
+      when Gosu::KB_ESCAPE
+        close
+      else
+        super
+      end
+    end
+
+
   end
 
   window = Window.new
